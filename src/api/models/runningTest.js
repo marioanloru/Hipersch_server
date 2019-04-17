c80f998758efb8d821987582bc7f853dc5aa2080
marioanloru <marioanloru@gmail.com>
2019-04-17 20:04:08 +0200

    Added testId to running test task

--- src/api/models/runningTest.js
+++ src/api/models/runningTest.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const runningTest = new Schema({
   athlete: { type: Schema.Types.ObjectId, ref: 'User' },
   distance: { type: Number, required: true },
-  testId: { type: String, required: true, unique: true }
+  testId: { type: String, required: true, unique: true },
   date: { type: Date, default: Date.now }
 });
 
