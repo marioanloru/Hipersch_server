8741bf6974321e0b36d4c5bd72e1cbdeacfdfbe7
marioanloru <marioanloru@gmail.com>
2019-05-01 20:11:51 +0200

    Range percentile added and working

--- src/api/utils/runningUtils.js
+++ src/api/utils/runningUtils.js
@@ -2,32 +2,58 @@ const dbUtils = require('./dbUtils');
 const runningTestModel = require('../models/runningTest');
 const clasificationsModel = require('../models/clasifications');
 const uuid4 = require('uuid4');
+const _ = require('lodash');
+const async = require('async');
 
-const mavVvO2maxRunningSamples = clasificationModel.find({ aspect: 'vvo2max', profile: 'running' });
-
-function processSixMinutesTest(distance, vo2maxIndirect) {
-  const speed = distance/360; 
+function processSixMinutesTest(distance, gender, vo2maxIndirect, mainCallback) {
+  const speedMS = distance/360;
+  const speedKMH = speedMS * 3.6; 
   const results = {
-    speed,
-    MAVvVo2max,
-    vo2max,
-    vat 
+    speed: speedKMH,
+    'MAVvVo2max': 0,
+    'vo2max': 0,
+    'vat': 0 
   };
-  
-  results.MAVvVo2max = percentil(10, speed, vVo2maxPorGenero) * 10;
-  //results.MAVvVo2max = percentilRank(mavVvO2maxRunningSamples, );
+
+  clasificationsModel
+    .find({ aspect: 'vvo2max', profile: 'running', gender })
+    .exec((err, res) => {
+      if (err) {
+        console.log(res);
+      } else {
+        let samples = _.sortBy(res, 'max');
+        let mavValues = [];
+        for (let i = 0; i < samples.length; i += 1) {
+          if (samples[i].min !== 0) {
+            mavValues.push(samples[i].min);
+          }
+          mavValues.push(samples[i].max);
+        }
+        async.waterfall([
+          (callback) => {
+            results.MAVvVo2max = percentilRank(mavValues, speedKMH) * 10;
+            callback(null);
+          }
+        ], (err, res) => {
+          console.log('RESULTADOS QUE DEVUELVOO', results);
+          mainCallback(null, results);
+        });
+      }
+    });
   //results.vo2max = percentil(vo2maxIndirect, vo2maxPorGenero) * 10;
   //results.vo2max = percentil((speed*85)/100, vUANPorGenero) * 10;
-
-  return results;
 }
 
-function percentilRank(samples, value, percentage) {
+function percentilRank(samples, value) {
   let valueInSample = false;
+  let valuePassed = false;
   let timesUnder = 0;
   let timesAbove = 0;
+  let valueAbove = 0;
+  let valueBelow = 0;
   let res = 0.0; 
 
+  //  Look for value bounds
   for (let i = 0; i < samples.length; i += 1) {
     if (samples[i] === value && !valueInSample) {
       valueInSample = true;
@@ -35,6 +61,15 @@ function percentilRank(samples, value, percentage) {
       if (samples[i] < value) {
         timesUnder += 1;
       } else if (samples[i] > value) {
+        if (!valuePassed ) {
+          valuePassed = true;
+          valueAbove = samples[i];
+          if (valueInSample) {
+            valueBelow = samples[i - 2];
+          } else {
+            valueBelow = samples[i - 1];
+          }
+        }
         timesAbove += 1;
       }
     }
@@ -43,34 +78,44 @@ function percentilRank(samples, value, percentage) {
   if (valueInSample) {
     res = timesUnder / (timesUnder + timesAbove);
   } else {
-    res = percentilRank(elvalorinferior) + 0.25*(percentilRank(cotainferior) - percentilRank(cotasuperior));
+    let percentileBelow = percentilRank(samples, valueBelow);
+    let percentileAbove = percentilRank(samples, valueAbove);
+    res = percentileBelow + 0.25*(percentileAbove - percentileBelow);
   }
     return res;
 }
 
 module.exports = {
   insertTestSixMinutes: (req, res) => {
-    const { distance } = req.body;
+    const { distance, gender } = req.body;
     const { userId } = req.user;
     
     //  AQUI PROCESAR TEST Y OBTENER INFO PARA EL MODELO
-    let result = processSixMinutesTest(distance);
-
-    const testToInsert = new runningTestModel({
-      distance,
-      athlete: userId,
-      testId: uuid4()
+    console.log('ANTES DE INSERTAR EL TEST!!');
+    let result = processSixMinutesTest(distance, gender, null, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('RESULTADO DE INSERTAR PROCESS SIX MINUTES TEST', result);
+        const testToInsert = new runningTestModel({
+          distance,
+          speed: result.speed,
+          athlete: userId,
+          testId: uuid4()
+        });
+        console.log(testToInsert);
+    
+        testToInsert
+          .save((err, data) => {
+            if (err) {
+              res.status(500).json(err);
+            } else {
+              res.status(200).json(data);
+            }
+          });
+      }
     });
-    console.log(testToInsert);
 
-    testToInsert
-      .save((err, data) => {
-        if (err) {
-          res.status(500).json(err);
-        } else {
-          res.status(200).json(data);
-        }
-      });
   },
   getUserTests: (req, res) => {
     const { userId } = req.user;
