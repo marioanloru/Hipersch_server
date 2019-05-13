99bb5662c678934dc99890b11025e33036244001
marioanloru <marioanloru@gmail.com>
2019-05-13 18:31:02 +0200

    Added mathUtils tests

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,74 @@
 const assert = require('assert');
-const supertest = require('supertest');
 const request = require('request');
 
-const app = require('../src/index');
-const userService = require('../src/api/users.service');
+const mathUtils = require('../src/api/utils/mathUtils');
+const runningUtils = require('../src/api/utils/runningUtils');
+//  const app = require('../src/index');
+//  const userService = require('../src/api/users.service');
 
-//  Library related tests
-describe('User tests', () => {
+describe('Math utils tests', () => {
+  it('Check percentile rank', () => {
+    let samples = [10.2, 11.7, 13.2, 14.6, 16, 17.5, 19, 20, 21.8, 22.2, 24];
+    let value = 18.00;
+    assert.equal(mathUtils.percentilRank(samples, value), 0.53);
+  });
+
+  it('Check linear interpolation', () => {
+    assert.equal(mathUtils.linearInterpolation(17.5, 18, 19, 0, 100), 33.33);
+  });
+});
+/*describe('Running tests', () => 
+  it('Check running six minutes test', (done) => {
+    //  Mock
+    const reqMock = {
+      body: {},
+      cookies: {},
+      query: {},
+      params: {},
+      user: {
+        gender: 'female',
+        userId: 'anuserid'
+      }
+    };
+    const resMock = {
+      clearCookie: spy(),
+      cookie: spy(),
+      download: spy(),
+      end: spy(),
+      format: spy(),
+      json: spy(),
+      jsonp: spy(),
+      redirect: spy(),
+      render: spy(),
+      send: spy(),
+      sendFile: spy(),
+      sendStatus: spy(),
+      set: spy(),
+      type: spy()
+    };
+    const context = {
+      distance: "1800"
+    };
+    runningUtils
+      .insertTestSixMinutes(req, res)
+      .then()
+    supertest(app)
+      .post('/api/running/test')
+      .set('Authorization', `Bearer ${token}`)
+      .send(context)
+      .expect('Content-type', /json/)
+      .expect(200)
+      .then(response => {
+        assert(response.body.MAVvVo2max, 7.33);
+        assert(response.body.vo2max, 7.33);
+        assert(response.body.vat, 7.33);
+        done();
+      });
+  })
+});*/
+
+//  User api tests [TO DO]
+/*describe('User tests', () => {
   //  Get test admin token
   /*before((done) => {
     request('localhost:9000')
@@ -19,7 +81,7 @@ describe('User tests', () => {
     });*/
   
   //  User creation
-  it('Check user register', (done) => {
+  /*it('Check user register', (done) => {
     const context = {
       "username": "mujer",
       "password": "finisterre",
@@ -61,7 +123,7 @@ describe('User tests', () => {
   });
 
   //  User admin creation error
-  /*it('Check user register', (done) => {
+  it('Check user register', (done) => {
     const context = {
       "username": "testAdmin",
       "password": "finisterre",
@@ -120,58 +182,5 @@ describe('User tests', () => {
       .expect(400, {
         message: 'You do not have permissions for this action. This action will be reported'
       }, done);
-  });*/
-});
-
-describe('Running tests', () => {
-  let token = '';
-  before((done) => {
-    const context = {
-      "username": "test",
-      "password": "test",
-      "firstName": "unamujer",
-      "lastName": "muymujer",
-      "gender": "female",
-      "bodyWeight": "52",
-      "height": "163",
-      "role": "user"
-    };
-
-    supertest(app)
-      .post('/api/user/register')
-      .send(context)
-      .end((err, res) => {
-        done();
-      });
   });
-  before((done) => {
-    const context = {
-      "username": "test",
-      "password": "test",
-    };
-    supertest(app)
-      .post('/api/user/login')
-      .send(context)
-      .end((err, res) => {
-        token = res.body.token;
-        done();
-      });
-  });
-  it('Check running six minutes test', (done) => {
-    const context = {
-      distance: "1800"
-    };
-    supertest(app)
-      .post('/api/running/test')
-      .set('Authorization', `Bearer ${token}`)
-      .send(context)
-      .expect('Content-type', /json/)
-      .expect(200)
-      .then(response => {
-        assert(response.body.MAVvVo2max, 7.33);
-        assert(response.body.vo2max, 7.33);
-        assert(response.body.vat, 7.33);
-        done();
-      });
-  })
-});
+});*/
